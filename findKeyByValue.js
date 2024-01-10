b58188608b43bff69fc68e8e13718d9cd8996889
Mike Dragert <mikedragert@gmail.com>
2024-01-10 13:35:52 -0500

    findKeyByValue Implemented

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearch, valueToFind) {
+  const pairs = Object.entries(objectToSearch);
+  for (const [key, value] of pairs) {
+    if (value === valueToFind) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// TEST CODE
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

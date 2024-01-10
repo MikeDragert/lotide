1c7b84d356b0e3a35601579d66ee05570cb277ca
Mike Dragert <mikedragert@gmail.com>
2024-01-10 14:22:02 -0500

    Implemented assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -5,7 +5,7 @@ const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 

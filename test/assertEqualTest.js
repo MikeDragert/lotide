77304bd309d2c99dd139c56922d1064325dc72cd
Mike Dragert <mikedragert@gmail.com>
2024-01-15 09:41:25 -0500

    export and tests for assertEqual and head

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual')
+// TEST CODE
+assertEqual("Lighthouse Labs","Bootcamp");
+assertEqual("MikeIsAwesome","MikeIsAwesome");
+assertEqual(1,1);
+assertEqual(10, 10.1);
+assertEqual(10, 5);
+assertEqual(4, "4");
